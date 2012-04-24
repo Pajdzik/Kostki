bc8daf70c9dc348c0bfa180d1857b7637ee80f7f
baszmen <mateusz.dembski@gmail.com>
2012-04-24 19:41:31 +0200

    Uporządkowanie kodu, stworzenie Panelu Controlnego (klasa) . Sprawdzanie czy wizualnie to będzie dobrze wyglądać, 70 px ,landscape i portrait . jest 20 kart w jednym miejscu na rozłożenie .

--- Kostki/Kostki/GamePage.xaml.cs
+++ Kostki/Kostki/GamePage.xaml.cs
@@ -12,39 +12,19 @@ using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using System.Windows.Media.Imaging;
 using System.Diagnostics;
+using Kostki.Class;
 
 namespace Kostki
 {
-    enum Colors
-    {
-        Blue,
-        Green,
-        Red,
-        Yellow
-    }
-
-    enum Figures
-    {
-        Club,
-        Diamond,
-        Heart,
-        Spade
-    }
-
-
-
     public partial class GamePage : PhoneApplicationPage
     {
-
-        private List<Image> listOfImages = new List<Image>();
         private Image clubs, diamond, heart, spade;
 
-        private Image[] currentCards;
-        private BitmapImage[,] cards;
+        private ControlPanel controlPanel;
 
         public GamePage()
         {
-
+            this.controlPanel = new ControlPanel();
             InitializeComponent();
             this.Init();
             this.Loaded += new RoutedEventHandler(GamePageLoaded);
@@ -52,89 +32,27 @@ namespace Kostki
 
         public void Init()
         {
-            this.clubs = new Image();
-            this.heart = new Image();
-            this.spade = new Image();
-            this.diamond = new Image();
-
-            currentCards = new Image[4];
-            cards = new BitmapImage[4, 4];
+            this.clubs = this.controlPanel.GetImageByColorAndId(Figures.Diamond, CardColors.Blue);
+            this.heart = this.controlPanel.GetImageByColorAndId(Figures.Diamond, CardColors.Blue);
+            this.spade = this.controlPanel.GetImageByColorAndId(Figures.Diamond, CardColors.Blue);
+            this.diamond = this.controlPanel.GetImageByColorAndId(Figures.Diamond, CardColors.Blue);
         }
 
         private void GamePageLoaded(object sender, RoutedEventArgs e)
         {
-            this.LoadCardsImage();
             this.showCards();
         }
 
-        ///
-        /// Loaded cards images
-        /// 
-        public void LoadCardsImage()
+        public void showCards() //tymczasowa funkcja
         {
-            cards[(int) Figures.Club, (int) Colors.Blue] = new BitmapImage(new Uri("/img/clubs/blue.png", UriKind.Relative));
-            cards[(int) Figures.Club, (int) Colors.Green] = new BitmapImage(new Uri("/img/clubs/green.png", UriKind.Relative));
-            cards[(int) Figures.Club, (int) Colors.Red] = new BitmapImage(new Uri("/img/clubs/red.png", UriKind.Relative));
-            cards[(int) Figures.Club, (int) Colors.Yellow] = new BitmapImage(new Uri("/img/clubs/yellow.png", UriKind.Relative));
-
-            cards[(int) Figures.Diamond, (int) Colors.Blue] = new BitmapImage(new Uri("/img/diamond/blue.png", UriKind.Relative));
-            cards[(int) Figures.Diamond, (int) Colors.Green] = new BitmapImage(new Uri("/img/diamond/green.png", UriKind.Relative));
-            cards[(int) Figures.Diamond, (int) Colors.Red] = new BitmapImage(new Uri("/img/diamond/red.png", UriKind.Relative));
-            cards[(int) Figures.Diamond, (int) Colors.Yellow] = new BitmapImage(new Uri("/img/diamond/yellow.png", UriKind.Relative));
-
-            cards[(int) Figures.Heart, (int) Colors.Blue] = new BitmapImage(new Uri("/img/heart/blue.png", UriKind.Relative));
-            cards[(int) Figures.Heart, (int) Colors.Green] = new BitmapImage(new Uri("/img/heart/green.png", UriKind.Relative));
-            cards[(int) Figures.Heart, (int) Colors.Red] = new BitmapImage(new Uri("/img/heart/red.png", UriKind.Relative));
-            cards[(int) Figures.Heart, (int) Colors.Yellow] = new BitmapImage(new Uri("/img/heart/yellow.png", UriKind.Relative));
-
-            cards[(int) Figures.Spade, (int) Colors.Blue]= new BitmapImage(new Uri("/img/spade/blue.png", UriKind.Relative));
-            cards[(int) Figures.Spade, (int) Colors.Green ] = new BitmapImage(new Uri("/img/spade/green.png", UriKind.Relative));
-            cards[(int) Figures.Spade, (int) Colors.Red] = new BitmapImage(new Uri("/img/spade/red.png", UriKind.Relative));
-            cards[(int) Figures.Spade, (int) Colors.Yellow] = new BitmapImage(new Uri("/img/spade/yellow.png", UriKind.Relative));
-
-            Random r = new Random();
-
-            this.clubs.Source = cards[r.Next(4), r.Next(4)];
-            this.clubs.Height = 70;
-            this.clubs.Width = 70;
-            this.diamond.Source = cards[r.Next(4), r.Next(4)];
-            this.diamond.Height = 70;
-            this.diamond.Width = 70;
-            this.heart.Source = cards[r.Next(4), r.Next(4)];
-            this.heart.Height = 70;
-            this.heart.Width = 70;
-            this.spade.Source = cards[r.Next(4), r.Next(4)];
-            this.spade.Height = 70;
-            this.spade.Width = 70;
-        }
-
-        public void showCards()
-        {
-
-            this.heart.ManipulationStarted += new EventHandler<ManipulationStartedEventArgs>(ManipulationStarted);
-            this.heart.ManipulationDelta +=new EventHandler<ManipulationDeltaEventArgs>(ManipulationDelta);
-            this.heart.ManipulationCompleted += new EventHandler<ManipulationCompletedEventArgs>(ManipulationCompleted);
-
-            this.spade.ManipulationStarted += new EventHandler<ManipulationStartedEventArgs>(ManipulationStarted);
-            this.spade.ManipulationDelta += new EventHandler<ManipulationDeltaEventArgs>(ManipulationDelta);
-            this.spade.ManipulationCompleted += new EventHandler<ManipulationCompletedEventArgs>(ManipulationCompleted);
-
-            this.diamond.ManipulationStarted += new EventHandler<ManipulationStartedEventArgs>(ManipulationStarted);
-            this.diamond.ManipulationDelta += new EventHandler<ManipulationDeltaEventArgs>(ManipulationDelta);
-            this.diamond.ManipulationCompleted += new EventHandler<ManipulationCompletedEventArgs>(ManipulationCompleted);
-
-            this.clubs.ManipulationStarted += new EventHandler<ManipulationStartedEventArgs>(ManipulationStarted);
-            this.clubs.ManipulationDelta += new EventHandler<ManipulationDeltaEventArgs>(ManipulationDelta);
-            this.clubs.ManipulationCompleted += new EventHandler<ManipulationCompletedEventArgs>(ManipulationCompleted);
-
-             this.canvas.Children.Add(heart);
-             this.canvas.Children.Add(spade);
-             this.canvas.Children.Add(diamond);
-             this.canvas.Children.Add(clubs);
-
-             Canvas.SetLeft(spade, 71);
-             Canvas.SetLeft(diamond, 142);
-             Canvas.SetLeft(clubs, 213);
+            for (int i = 0; i < 20; i++) 
+            {
+                Image image = this.controlPanel.GetImageByColorAndId(Figures.Diamond, CardColors.Blue);
+                image.ManipulationStarted += new EventHandler<ManipulationStartedEventArgs>(ManipulationStarted);
+                image.ManipulationDelta += new EventHandler<ManipulationDeltaEventArgs>(ManipulationDelta);
+                image.ManipulationCompleted += new EventHandler<ManipulationCompletedEventArgs>(ManipulationCompleted);
+                this.canvas.Children.Add(image);
+            }
         }
 
 
@@ -147,33 +65,15 @@ namespace Kostki
         private void ManipulationDelta(object sender, ManipulationDeltaEventArgs e)
         {
             Image image = (Image)sender;
-
-            Debug.WriteLine(e.DeltaManipulation.Translation.X + " " + e.DeltaManipulation.Translation.Y);
-
-            double x = Canvas.GetLeft(image);
-            double y = Canvas.GetTop(image);
-
-            Canvas.SetLeft(image, x + e.DeltaManipulation.Translation.X);
-            Canvas.SetTop(image, y + e.DeltaManipulation.Translation.Y); 
-
-            Debug.WriteLine("w trakcie");
-
+            Canvas.SetLeft(image, Canvas.GetLeft(image) + e.DeltaManipulation.Translation.X);
+            Canvas.SetTop(image, Canvas.GetTop(image) + e.DeltaManipulation.Translation.Y); 
         }
 
         private void ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
         {
             Image image = (Image) sender;
-            double x = Canvas.GetLeft(image);
-            double y = Canvas.GetTop(image);
-
-            x = 50 * ((int)x / 50);             // pseudozaokrąglanie do "siatki"
-            y = 50 * ((int)y / 50);
-
-            Canvas.SetLeft(image, x);
-            Canvas.SetTop(image, y); 
-
-            Debug.WriteLine("end");
-
+            Canvas.SetLeft(image, (double)((int)(Canvas.GetLeft(image) / this.controlPanel.GetFieldSize()) * this.controlPanel.GetFieldSize())); //pseudozaokrąglenie do 70
+            Canvas.SetTop(image, (double)((int)(Canvas.GetTop(image) / this.controlPanel.GetFieldSize()) * this.controlPanel.GetFieldSize()));
         }
     }
 }
